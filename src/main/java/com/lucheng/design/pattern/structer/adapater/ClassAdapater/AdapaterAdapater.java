939eb3b40e4093bb1b5730d5a3ca3dd19b23c9bb
lucheng28 <lucheng28@jd.com>
2019-11-13 14:09:31 +0800

    类适配器模式

--- src/main/java/com/lucheng/design/pattern/structer/adapater/ClassAdapater/AdapaterAdapater.java
+++ src/main/java/com/lucheng/design/pattern/structer/adapater/ClassAdapater/AdapaterAdapater.java
@@ -8,7 +8,9 @@ public class AdapaterAdapater extends Adapatee implements Target {
     @Override
     public void request() {
         //可增加相关的适配逻辑
+        System.out.println("适配相关逻辑前");
         super.AdataperRequest();
+        System.out.println("适配相关逻辑后");
         //可增加相关的适配逻辑
     }
 }
---
+++ src/main/java/com/lucheng/design/pattern/structer/adapater/ClassAdapater/Test.java
@@ -0,0 +1,8 @@
+package com.lucheng.design.pattern.structer.adapater.ClassAdapater;
+
+public class Test {
+    public static void main(String[] args) {
+        AdapaterAdapater adapaterAdapater = new AdapaterAdapater();
+        adapaterAdapater.request();
+    }
+}
